5a96b9255f3ae67c604bc7a8aba03189c8d466b3
bryandaniswara <bryan.daniswara@gmail.com>
2020-09-10 10:49:12 +0700

    Init Vue

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello, world! {{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "from vue",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
