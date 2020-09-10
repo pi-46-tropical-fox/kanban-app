4535fe17055a41578a8c03f385a621cf7545b14e
Fandi Ario <fandi.ario10@gmail.com>
2020-09-10 13:02:24 +0700

    Release 2.2 -Add CRUDE To Task

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -3,18 +3,79 @@ const {Task} = require ("../models")
 class TaskController {
 
     static addTask (req, res, next) {
+        let params = {
+            title : req.body.title,
+            CategoryId : req.body.CategoryId, 
+            UserId : req.UserData.id
+        }
+
+        Task.create (params)
+
+        .then (data => {
+            return res.status (201).json (data)
+        })
+
+        .catch (err => {
+            console.log (err)
+            return res.status (400).json ({message : "Bad Request - Error Validation"})
+        })
 
     }
 
     static getTask (req, res, next) {
+        Task.findAll ()
+
+        .then (data => {
+            return res.status (200).json (data)
+        })
+
+        .catch (err => {
+            console.log (err)
+            return res.status (500).json ({message : "Internal Server Error"})
+
+        })
 
     }
 
     static editTask (req, res, next) {
+        let params = {
+            title : req.body.title,
+            CategoryId : req.body.CategoryId
+        }
+
+        Task.update (params, {
+            where : {id : req.params.id}
+        })
+
+        .then (data => {
+            if (!data) {
+                return res.status (404).json ({message : "Data is Not Found"})
+            
+            } else {
+                return res.status (200).json ({message : "Data has been Updated"})
+            }
+        })
+
+        .catch (err => {
+            console.log (err)
+            return res.status (400).json ({message : "Bad Request - Error Validation"})
+        })
 
     }
 
     static deleteTask (req, res, next) {
+        Task.destroy ({
+            where : {id : req.params.id}
+        })
+
+        .then (data => {
+            return res.status (200).json ({message : "Data has been Deleted"})
+        })
+
+        .catch (err => {
+            console.log (err)
+            return res.status (500).json ({message : "Internal Server Error"})
+        })
 
     }
 
