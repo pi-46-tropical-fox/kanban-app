8b36671db64eed8f5170843ffa2eca76838002b8
unknown <izra.arzi@gmail.com>
2020-09-11 21:05:29 +0700

    prepare for deployment

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
+if (process.env.NODE_ENV != 'production') {
+    require('dotenv').config()
+}
 const express = require('express')
 const app = express()
 const route = require('./routes')
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 const {errHandler} = require('./middlewares/errHandler')
 
