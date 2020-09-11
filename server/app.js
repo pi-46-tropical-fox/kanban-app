c2ace23028c87d879b8aadb1b32f8748cdba8b7e
Imam Rabbani <imamrbbn@gmail.com>
2020-09-12 00:46:17 +0700

    deploy 4

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development'){
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 const router = require('./router')
 const errHandler = require('./middlewares/errHandler.js')
 const cors = require('cors')
