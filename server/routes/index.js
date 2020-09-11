146b67bd1b2f419e9995db8049aa18cddd2c8f69
arrizalrahmat <arrizalrahmat@gmail.com>
2020-09-11 22:03:03 +0700

    config db

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,9 @@ const route = require('express').Router()
 const userRoute = require('./userRoute.js')
 const taskRoute = require('./taskRoute')
 
+route.get('/', (req, res) => {
+    res.send('hello gais')
+})
 route.use('/users', userRoute)
 route.use('/tasks', taskRoute)
 
