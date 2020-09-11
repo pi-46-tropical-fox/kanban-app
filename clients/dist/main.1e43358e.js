484754ba5fb4c5979affcc2893352c09cfad7bd5
juanbrundism <juanbrundism@gmail.com>
2020-09-11 23:46:46 +0700

    deployed

--- clients/dist/main.1e43358e.js
+++ clients/dist/main.1e43358e.js
@@ -10345,7 +10345,7 @@ var _axios = _interopRequireDefault(require("axios"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var instance = _axios.default.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://kanban-juan-ganteng.herokuapp.com'
 });
 
 var _default = instance;
@@ -19150,7 +19150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57171" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54243" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
