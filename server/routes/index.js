54029c556e5e88dba89d61f1aae2f9958bfbf2eb
abdulfattah94 <abdulfattah.1994@gmail.com>
2020-09-12 00:51:31 +0700

    edited index route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,14 +4,15 @@ const {authentication} = require('../middleware/auth')
 const taskRouter = require('./task')
 const categoryRouter = require('./category')
 
-router.get('/', (req, res) => {
-    res.send('home')
-})
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.post('/googleLogin', UserController.googleLogin)
 
 router.use(authentication)
+
+router.get('/', (req, res) => {
+    res.send('home')
+})
 
 router.use('/categories',categoryRouter)
 router.use('/tasks',taskRouter)
