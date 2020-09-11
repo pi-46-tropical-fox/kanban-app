7e53f80a0f425eca3404c156f9a507518f956a16
mohammad rendra <abuabdillatief@gmail.com>
2020-09-11 22:26:28 +0700

    prepare for deploy

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const routes = require("./routes");
 //================================
+app.get('/', (req, res) => {
+    res.send('Halo')
+})
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
