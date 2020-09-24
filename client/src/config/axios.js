3cbd09d33afbfc230f6da8f14b9f94789e3c1f40
Ahmad Hafizh <67890288+rafianaji@users.noreply.github.com>
2020-09-25 06:50:48 +0700

    Update axios.js

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://first-kanban-app.herokuapp.com'
 });
 
 export default instance
