95c76f30f4c5784bdafa6dd5861f0008a9732693
adiet95 <diezzyshop@gmail.com>
2020-09-25 10:52:16 +0700

    Sett DB

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+if(process.env.NODE_ENV === 'development'){
+  require('dotenv').config()
+}
+
 const express = require('express')
 const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
--- server/config/config.js
+++ server/config/config.js
@@ -18,10 +18,6 @@ module.exports = {
     "operatorsAliases": false
   },
   "production": {
-    "username": "osmjvpdtvxwijo",
-    "password": "d4ce00bf41f1b1724d63cf95adc2e3524adcc4eebb79adde01219510b1e8c7ba",
-    "database": "d4ibuuqa1sfjp0",
-    "host": "ec2-52-71-85-210.compute-1.amazonaws.com",
-    "dialect": "postgres",
-  }
+    "use_env_variable": "DATABASE_URL"
+   }
 }
